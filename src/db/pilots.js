09767fc29251a144ea7f5f312d259227ff032641
moraamasr5-create <moraamasr5@gmail.com>
2026-05-22 06:28:59 +0300

    Delete pilots.js

--- src/db/pilots.js
+++
@@ -1,5 +0,0 @@
-export const INITIAL_PILOTS = [
-    { id: 'p1', name: 'تامر عزت', state: 'available', shiftStatus: 'closed', phone: '01006354583', vehicle: 'موتوسيكل', shift: '10:00 am - 8:0 pm' },
-    { id: 'p2', name: 'صلاح', state: 'available', shiftStatus: 'closed', phone: '01271130280', vehicle: 'موتوسيكل', shift: '12:00 pm - 10:00 pm' },
-    { id: 'p3', name: 'أحمد سمير', state: 'available', shiftStatus: 'closed', phone: '01129364795', vehicle: 'موتوسيكل', shift: '8:00a m - 6:00 pm' },
-];
