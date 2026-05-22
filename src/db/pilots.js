0b5b0289f37b3e3a426c28e6f338995768d436bb
moraamasr5-create <moraamasr5@gmail.com>
2026-05-22 06:26:38 +0300

    Update pilots.js

--- src/db/pilots.js
+++ src/db/pilots.js
@@ -1,10 +1,5 @@
 export const INITIAL_PILOTS = [
-    { id: 'p1', name: 'تامر عزت', state: 'available', shiftStatus: 'closed', phone: '01006354583', vehicle: 'موتوسيكل', shift: '10:00A - 8:00P' },
-    { id: 'p2', name: 'صلاح', state: 'available', shiftStatus: 'closed', phone: '01271130280', vehicle: 'موتوسيكل', shift: '12:00P - 10:00P' },
-    { id: 'p3', name: 'سمير', state: 'available', shiftStatus: 'closed', phone: '01129364795', vehicle: 'موتوسيكل', shift: '8:00A - 6:00P' },
-    { id: 'p4', name: 'إبراهيم حسني', state: 'available', shiftStatus: 'closed', phone: '01108792146', vehicle: 'موتوسيكل', shift: '2:00P - 12:00A' },
-    { id: 'p5', name: 'أسلام ممدوح', state: 'available', shiftStatus: 'closed', phone: '01115034659', vehicle: 'موتوسيكل', shift: '10:00A - 8:00P' },
-    { id: 'p6', name: 'الجمل', state: 'available', shiftStatus: 'closed', phone: '01280278461', vehicle: 'موتوسيكل', shift: '12:00P - 10:00P' },
-    { id: 'p7', name: 'محمد أيمن', state: 'available', shiftStatus: 'closed', phone: '01107409359', vehicle: 'موتوسيكل', shift: '8:00A - 6:00P' },
-    { id: 'p8', name: 'زياد', state: 'available', shiftStatus: 'closed', phone: '01144585833', vehicle: 'موتوسيكل', shift: '2:00P - 12:00A' }
+    { id: 'p1', name: 'تامر عزت', state: 'available', shiftStatus: 'closed', phone: '01006354583', vehicle: 'موتوسيكل', shift: '10:00 am - 8:0 pm' },
+    { id: 'p2', name: 'صلاح', state: 'available', shiftStatus: 'closed', phone: '01271130280', vehicle: 'موتوسيكل', shift: '12:00 pm - 10:00 pm' },
+    { id: 'p3', name: 'أحمد سمير', state: 'available', shiftStatus: 'closed', phone: '01129364795', vehicle: 'موتوسيكل', shift: '8:00a m - 6:00 pm' },
 ];
